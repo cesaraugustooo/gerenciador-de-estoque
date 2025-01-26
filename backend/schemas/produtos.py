7f715fb6b9801fed5730f88233adf9803a2d18ad
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-01-26 16:48:57 -0300

    Atualização na entrada e saida dos dados

--- backend/schemas/produtos.py
+++ backend/schemas/produtos.py
@@ -1,7 +1,7 @@
-from pydantic import BaseModel,Fiel
+from pydantic import BaseModel
 from schemas.categorias import *
 from schemas.fornecedores import *
-
+from schemas.usuarios import *
 
 class ProdutoReponse(BaseModel):
     id : int
@@ -9,10 +9,12 @@ class ProdutoReponse(BaseModel):
     preco : float
     fornecedor_id : FornecedorReponse
     categoria_id : CategoriaReponse
+    usuario_id : UsuarioResponse    
 
 class ProdutoCreate(BaseModel):
     nome : str
     preco : float
     fornecedor_id : int
     categoria_id : int
+    usuario_id : int
 
