36457ce0a477dabd433d27d32052fffdc50d268b
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-01-26 16:47:30 -0300

    Adição de usuario como chave estrangeira

--- backend/models/produtos.py
+++ backend/models/produtos.py
@@ -1,6 +1,7 @@
 from peewee import CharField,IntegerField,ForeignKeyField,Model,AutoField,DecimalField
 from models.categorias import CategoriaDB
 from models.fornecedores import FornecedorDB
+from models.usuarios import UsuarioDB
 from config.database import database
 
 class ProdutosDB(Model):
@@ -9,7 +10,7 @@ class ProdutosDB(Model):
     preco = DecimalField()
     fornecedor_id = ForeignKeyField(FornecedorDB,backref='fornecedor')
     categoria_id = ForeignKeyField(CategoriaDB, backref='categoria')
-
+    usuario_id = ForeignKeyField(UsuarioDB,backref='usuario')
     class Meta:
         database = database
 
