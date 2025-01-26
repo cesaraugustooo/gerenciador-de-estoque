26b7a2b04704efae9c3ac1b4bdadde99e92b27e9
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-01-26 16:46:05 -0300

    Adição de rotas

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 from routers.categorias import router as categoria_routers
 from config.database import end_connect,connect_database
-
+from routers.fornecedores import router as fornecedores_routers
+from routers.produtos import router as produtos_router
 app = FastAPI()
 
 app.add_event_handler(event_type='startup',func=connect_database)
 app.add_event_handler(event_type='shutdown',func=end_connect)
 
 app.include_router(categoria_routers)
+app.include_router(fornecedores_routers)
+app.include_router(produtos_router)
+
 
