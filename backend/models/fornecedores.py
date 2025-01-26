a4c2d13dc8ee55a59a8a9cf14a39927f7992ffa4
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-01-26 16:46:19 -0300

    Correção

--- backend/models/fornecedores.py
+++ backend/models/fornecedores.py
@@ -1,12 +1,13 @@
 from peewee import CharField,IntegerField,ForeignKeyField,Model,AutoField,DecimalField
 from config.database import database
+from models.usuarios import UsuarioDB
 
 class FornecedorDB(Model):
     id = AutoField()
     nome = CharField()
     email = CharField()
     numero = CharField()
+    usuario_id = ForeignKeyField(UsuarioDB,backref='usuarioo')
 
     class Meta:
         database = database
-
